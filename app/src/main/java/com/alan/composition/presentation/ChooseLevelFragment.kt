63c0aa6c185bada5d29a08380d30eed62f8f8f9d
klementev <assssseeeee@gmail.com>
2023-02-07 12:30:42 +0300

    adding dataBinding to GameFinishedFragment.kt

--- app/src/main/java/com/alan/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/alan/composition/presentation/ChooseLevelFragment.kt
@@ -5,16 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.alan.composition.R
+import androidx.navigation.fragment.findNavController
 import com.alan.composition.databinding.FragmentChooseLevelBinding
 import com.alan.composition.domain.entity.Level
 
 class ChooseLevelFragment : Fragment() {
-
     private var _binding: FragmentChooseLevelBinding? = null
     private val binding: FragmentChooseLevelBinding
         get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding = null")
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,22 +42,15 @@ class ChooseLevelFragment : Fragment() {
     }
 
     private fun launchGameFragment(level: Level) {
-        requireActivity().supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.main_container, GameFragment.newInstance(level))
-            .addToBackStack(GameFragment.NAME)
-            .commit()
+        findNavController().navigate(
+            ChooseLevelFragmentDirections.actionChooseLevelFragmentToGameFragment(
+                level
+            )
+        )
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    companion object {
-        const val NAME = "ChooseLevelFragment"
-        fun newInstance(): ChooseLevelFragment {
-            return ChooseLevelFragment()
-        }
     }
 }
